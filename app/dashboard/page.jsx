5ca2c01af8d55a6afc3b174bed4b3028178db850
MrSreehari-S <sreehari200411@gmail.com>
2026-01-19 23:21:57 +0530

    clerk user auth

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import AuthUserButton from "@/components/AuthUserButton";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <AuthUserButton />
+    </>
+  );
+}
